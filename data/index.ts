9c909d3dc928b14a19292a68eeda078f35b367ad
Kashan Haider <19cs162@jssaten.ac.in>
2024-10-24 16:28:31 +0530

    Final Commit

---
+++ data/index.ts
@@ -0,0 +1,200 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Experience", link: "#experience" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title:
+      " I Prioritize working closely with clients to foster transparent communication. ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify",
+    img: "/b110.jpg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Flexible with time zone communication",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "Experties",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech-driven professional focused on optimizing airport systems.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently bringing DXN to Life!",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-start md:justify-start lg:justify-start",
+    img: "",
+    spareImg: "/FV_NIA.jpg",
+  },
+  {
+    id: 6,
+    title: "Ready to collaborate on your next project ?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Noida International Airport",
+    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "/ui.earth.com",
+  },
+  // {
+  //   id: 2,
+  //   title: "Yoom - Video Conferencing App",
+  //   des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+  //   img: "/p2.svg",
+  //   iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+  //   link: "/ui.yoom.com",
+  // },
+  // {
+  //   id: 3,
+  //   title: "AI Image SaaS - Canva Application",
+  //   des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+  //   img: "/p3.svg",
+  //   iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+  //   link: "/ui.aiimg.com",
+  // },
+  // {
+  //   id: 4,
+  //   title: "Animated Apple Iphone 3D Website",
+  //   des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+  //   img: "/p4.svg",
+  //   iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+  //   link: "/ui.apple.com",
+  // },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "As Kashan’s mentor during the Noida International Airport (NIA) project, I had observed his growth and expertise as an Integration Engineer. Kashan was a key contributor to the Master Systems Integration of MEP, ELV, ICT, and other critical airport systems. His ability to effectively manage requirement gathering, interface design, testing, and problem-solving was invaluable to the project. Kashan’s technical knowledge, coupled with his precision in Technical documentation and his commitment to meeting deliverables, consistently exceeded expectations. I have no doubt that he will continue to be an outstanding asset to any team. I Highly  recommended him",
+    name: "Mohammed Abdul Imran",
+    title: "Aviation Expert",
+    img: "/imran_sir.png",
+  },
+  {
+    quote:
+      "I am pleased to recommend Kashan, who is highly task-oriented and works closely with all stakeholders while maintaining a strong focus on detail and clear communication. I had the opportunity to work with him on the Noida International Airport project, where he was responsible for overseeing the integration of various systems at NIA. His dedication, hard work, and attention to detail were truly impressive. I highly appreciate his contributions and wish him continued success in all his future endeavors.",
+    name: "Chetan Anand",
+    title: "Manager - Design & Engineering",
+    img: "/chetan.jpeg",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "Siemens",
+    img: "/s.webp",
+    nameImg: "",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Integration Engineer with Source Integreta / ICAD Airports",
+    desc: "As an Integration Engineer working on Master Systems Integration at Noida International Airport, I ensure seamless integration and reliability of critical airport IT and communication systems to enhance operational efficiency.",
+    className: "md:col-span-2",
+    thumbnail: "/ap-1.png",
+  },
+  {
+    id: 2,
+    title: "B.Tech CSE | JSSATE Noida",
+    desc: "I earned my B.Tech in Computer Science Engineering from JSSATE Noida, graduating among the top ranks in my department with a CGPA of 9.0.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/cse.png",
+  },
+  {
+    id: 3,
+    title: "Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+  {
+    id: 4,
+    title: "AISSCE | Aditya Birla Public School",
+    desc: "I completed my senior secondary education in the Physics, Chemistry, and Mathematics stream at Aditya Birla Public School, Jagdishpur, achieving a score of 83.4%.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/school.png",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/kashanhz999",
+  },
+  {
+    id: 2,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/kashan-haider110/",
+  },
+];
